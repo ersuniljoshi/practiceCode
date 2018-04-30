e7b2d28681259dbfa1f96b9d1531968fc49f316a
Sunil Joshi <joshi.sunil@outlook.com>
2018-04-30 23:24:57 +0530

    decorators

---
+++ DSAInPython/decorators.py
@@ -0,0 +1,18 @@
+def bold(fn):
+    def karbold(*args):
+        return "<b>" + fn(*args) + "</b>"
+    return karbold
+
+
+def italic(fn):
+    def karitalic(*args):
+        return "<i>" + fn(*args) + "</i>"
+    return karitalic
+
+
+@bold
+@italic
+def html(string):
+    return string
+
+print html("Jackpot,Lag gya")
