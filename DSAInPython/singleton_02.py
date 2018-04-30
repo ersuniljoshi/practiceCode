885f0581e9b27404a0725a044f0188a039635f66
Sunil Joshi <joshi.sunil@outlook.com>
2018-04-30 23:23:41 +0530

    singleton with decorators

---
+++ DSAInPython/singleton_02.py
@@ -0,0 +1,16 @@
+def singleton(myClass):
+    instances = {}
+
+    def getInstance(*args, **kwargs):
+        if myClass not in instances:
+            instances[myClass] = myClass(*args, **kwargs)
+            return instances[myClass]
+    return getInstance
+
+
+@singleton
+class TestClass(object):
+    pass
+
+x = TestClass()
+print x
