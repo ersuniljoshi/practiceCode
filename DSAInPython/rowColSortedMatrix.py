6030a4b618e08d6fd09e962487f518c4fcdf5e88
Sunil Joshi <sunilj@surveymonkey.com>
2018-05-15 17:00:07 +0530

    More Problem Solved

---
+++ DSAInPython/rowColSortedMatrix.py
@@ -0,0 +1,28 @@
+M = [
+    [10, 20, 30, 40],
+    [15, 25, 35, 45],
+    [27, 29, 37, 48],
+    [32, 33, 39, 50]
+]
+
+num = 99
+
+rows = len(M)
+columns = len(M[0])
+
+tc = columns - 1
+tr = 0
+found = False
+while (tr < rows- 1) and tc >= 0:
+    print "Debug", tr, tc
+    if M[tr][tc] == num:
+        found = True
+        print tr, tc
+        break
+    if num > M[tr][tc]:
+        tr += 1
+    if num < M[tr][tc]:
+        tc -= 1
+
+if not found:
+    print "Element Doesnot Exists"
---
+++ DSAInPython/seggEvenOdd.py
@@ -0,0 +1,19 @@
+a = [1, 9, 5, 3, 2, 6, 7, 11]
+a= [1, 3, 2, 4, 7, 6, 9, 10]
+l = 0
+r = len(a) -1
+print "Before", a
+while l < r:
+    if a[l] % 2 == 0:
+        l += 1
+    else:
+        if a[r] % 2 == 0:
+            a[l], a[r] = a[r], a[l]
+            l += 1
+            r -= 1
+        else:
+            r -= 1
+
+print "After", a
+
+
